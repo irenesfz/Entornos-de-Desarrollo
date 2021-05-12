1aa93b1cfd61b1cba63d3a3cad580ea4ac6547b0
irenesfz <76596590+irenesfz@users.noreply.github.com>
2021-05-12 11:23:44 +0200

    Delete IreneSanchez-Actividades1-UT5/actividad1-IreneSanchez directory

--- IreneSanchez-Actividades1-UT5/actividad1-IreneSanchez/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['adquirirnombre_9',['adquirirNombre',['../classpersona.html#a481d7d102bcf1366d9bdde3fcec2dd75',1,'persona']]]
-];
--- IreneSanchez-Actividades1-UT5/actividad1-IreneSanchez/html/search/functions_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['hablar_13',['hablar',['../classpersona.html#ac63ac8908b3ec79de7455b88b0ad8453',1,'persona']]]
-];
